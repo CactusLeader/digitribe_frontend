fbdac9f2f171644391b1fd6a4e4174929395993d
Elodie Morel <elodie.morel08@gmail.com>
2022-02-28 17:17:26 +0100

    ajout des POI sur la carte

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Text, View, ImageBackground, TouchableOpacity } from "react-native";
 import { Button, Overlay, Input } from "react-native-elements";
-import { Entypo } from '@expo/vector-icons'; 
+import { Entypo } from "@expo/vector-icons";
 
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
@@ -11,9 +11,13 @@ import * as Permissions from "expo-permissions";
 function MapScreen() {
   const [currentLatitude, setCurrentLatitude] = useState(48.866667);
   const [currentLongitude, setCurrentLongitude] = useState(2.333333);
+  const [addPOI, setAddPOI] = useState(false);
+  const [listPOI, setListPOI] = useState([]);
 
   console.log("currentLattitude", currentLatitude);
   console.log("currentLongitude", currentLongitude);
+  console.log("addPOI", addPOI);
+  console.log("listPOI", listPOI);
 
   useEffect(() => {
     async function askPermissions() {
@@ -33,38 +37,73 @@ function MapScreen() {
     askPermissions();
   }, []);
 
+  onPressButton = () => {
+    setAddPOI(true);
+  };
+
+  onPressScreen = (evt) => {
+    console.log("evt.nativeEvent", evt.nativeEvent);
+    const lat = evt.nativeEvent.coordinate.latitude;
+    const long = evt.nativeEvent.coordinate.longitude;
+    console.log("lat", lat);
+    console.log("long", long);
+    if (addPOI) {
+      setListPOI([...listPOI, { lat, long }]);
+    }
+  };
+
+  const tabListPOI = listPOI.map((poi, index) => {
+    return (
+      <Marker
+        key={index}
+        coordinate={{
+          latitude: poi.lat,
+          longitude: poi.long,
+        }}
+        pinColor="#FFD440"
+      />
+    );
+  });
+
   return (
-    <MapView
+    <View
       style={{
         flex: 1,
-        alignItems: "flex-end",
-        justifyContent: "flex-end",
       }}
     >
-      <Marker
-        coordinate={{
-          latitude: currentLatitude,
-          longitude: currentLongitude,
+      <MapView
+        onPress={(evt) => onPressScreen(evt)}
+        style={{
+          flex: 1,
+          alignItems: "flex-end",
+          justifyContent: "flex-end",
         }}
-        title="Hello"
-        description="I am here"
-        pinColor="#8525FF"
-      />
-
+      >
+        <Marker
+          coordinate={{
+            latitude: currentLatitude,
+            longitude: currentLongitude,
+          }}
+          title="Hello"
+          description="I am here"
+          pinColor="#8525FF"
+        />
+        {tabListPOI}
+      </MapView>
       <View
         style={{
-          alignSelf: "flex-end",
+          position: "absolute",
+          bottom: 15,
+          right: 15,
         }}
       >
         <Button
           icon={{
-            name: 'plus',
-            type: 'entypo',
+            name: "plus",
+            type: "entypo",
             size: 35,
-            color: 'white',
+            color: "white",
           }}
-          loading={false}
-          loadingProps={{ size: "small", color: "white" }}
           buttonStyle={{
             backgroundColor: "#FFD440",
             borderRadius: 100,
@@ -73,15 +112,14 @@ function MapScreen() {
             width: 65,
             height: 65,
           }}
-         
           containerStyle={{
             marginHorizontal: 30,
             marginVertical: 30,
           }}
-          onPress={() => console.log("aye")}
+          onPress={() => onPressButton()}
         />
       </View>
-    </MapView>
+    </View>
   );
 }
 
