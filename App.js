58e9b56577a4dcb39a51450aa08366ea701af19c
Maxime Gomez-Duret <maxime.gomezduret@gmail.com>
2022-02-28 16:28:47 +0100

    add Profile creation screen

--- App.js
+++ App.js
@@ -14,11 +14,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-        {/* <Stack.Screen
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
           name="AccountCreation"
           component={AccountCreationScreen}
-        /> */}
+        />
         <Stack.Screen name="Focus" component={FocusScreen} />
         <Stack.Screen
           name="ProfileCreation"
--- components/ProfileCreationScreen.js
+++ components/ProfileCreationScreen.js
@@ -1,15 +1,91 @@
-import React from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  View,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { Button } from "react-native-elements";
+import * as ImagePicker from "expo-image-picker";
+import { Picker } from "@react-native-picker/picker";
 
-export default function ProfileCreationScreen() {
+export default function ImagePickerExample() {
+  const [image, setImage] = useState(null);
+  const [description, setDescription] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState();
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const handleDescription = (val) => {
+    setDescription(val);
+  };
+
   return (
-    <View style={styles.container}>
-      <TextInput value="" placeholder="Username" />
-      <TextInput value="" placeholder="Last Name" />
-      <TextInput value="" placeholder="First Name" />
-      <TextInput value="" placeholder="Email" />
-      <TextInput value="" placeholder="Password" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.innerHeader}>Profil</Text>
+        </View>
+        <View style={styles.avatar}>
+          {!image ? (
+            <Image
+              source={require("../assets/profile_avatar.png")}
+              style={{ width: 100, height: 100 }}
+            />
+          ) : (
+            <Image
+              source={{ uri: image }}
+              style={{ width: 100, height: 100 }}
+            />
+          )}
+          <Button
+            title="+ Add Picture"
+            buttonStyle={styles.button}
+            onPress={pickImage}
+          />
+        </View>
+        <TextInput
+          multiline
+          value={description}
+          onChangeText={(val) => handleDescription(val)}
+          placeholder="Description"
+          style={styles.input}
+          maxLength={600}
+        />
+        <View style={styles.picker}>
+          <Picker
+            selectedValue={selectedLanguage}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedLanguage(itemValue)
+            }
+            mode="dropdown"
+            //   style={styles.picker}
+          >
+            <Picker.Item label="Please select your language" value="Unknown" />
+            <Picker.Item label="Français" value="Français" />
+            <Picker.Item label="English" value="English" />
+          </Picker>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -18,6 +94,46 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+  },
+  header: {
+    marginTop: "12%",
+    // alignItems: "center",
+  },
+  avatar: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    marginLeft: "10%",
+    marginBottom: "8%",
+  },
+  innerHeader: {
+    color: "#8525FF",
+    fontSize: 28,
+    fontWeight: "700",
+    marginBottom: 48,
+    // alignItems: "center",
+  },
+  button: {
+    width: "auto",
+    marginLeft: "10%",
+    // justifyContent: "center",
+    backgroundColor: "#8525FF",
+  },
+  input: {
+    height: "25%",
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: "85%",
+  },
+  text: {
+    marginTop: "5%",
+    marginLeft: "10%",
+    fontSize: 16,
+  },
+  picker: {
+    width: "85%",
     justifyContent: "center",
+    // alignItems: "center",
   },
 });
