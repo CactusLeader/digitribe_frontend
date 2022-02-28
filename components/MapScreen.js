be27a97bc62fe6da84c0b12244aa7b4ee6b7fd29
Elodie Morel <elodie.morel08@gmail.com>
2022-02-28 11:54:33 +0100

    géolocalisation avec marker

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Text, View, ImageBackground } from "react-native";
 import MapView from "react-native-maps";
+import {Marker} from 'react-native-maps';
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 
 function MapScreen() {
+
+    const [currentLatitude, setCurrentLatitude] = useState(48.866667);
+    const [currentLongitude, setCurrentLongitude] = useState(2.333333);
+
+    console.log('currentLattitude', currentLatitude)
+    console.log('currentLongitude', currentLongitude)
 
     useEffect(() => {
         async function askPermissions() {
           var permissions = await Permissions.askAsync(Permissions.LOCATION);
           console.log('permissions', permissions)
           if (permissions.status === 'granted') {
-            var location = await Location.getCurrentPositionAsync({});
-            console.log('location',location)
+            await Location.watchPositionAsync(
+                {distanceInterval :10},
+                (location) => {
+                    console.log('location',location)
+                    setCurrentLatitude(location.coords.latitude)
+                    setCurrentLongitude(location.coords.longitude)
+                });
           }
         }
         askPermissions();
@@ -20,13 +32,18 @@ function MapScreen() {
 
   return (
       
-<MapView style={{flex : 1}}
-    initialRegion={{
-    latitude: 37.78825,  // pour centrer la carte
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,  // le rayon à afficher à partir du centre
-    longitudeDelta: 0.0421,
-}} />
+<MapView style={{flex : 1}}>
+<Marker
+          coordinate={{
+            latitude: currentLatitude,
+            longitude: currentLongitude,
+          }}
+          title="Hello"
+          description="I am here"
+        />
+
+</MapView>
+    
   );
 }
 
