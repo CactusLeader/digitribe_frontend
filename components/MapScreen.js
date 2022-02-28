0bdd0866d3861598d251fc8f63a7e883b52da1c6
Elodie Morel <elodie.morel08@gmail.com>
2022-02-28 15:42:08 +0100

    button maj avec icon

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,77 +1,87 @@
 import React, { useEffect, useState } from "react";
 import { Text, View, ImageBackground, TouchableOpacity } from "react-native";
 import { Button, Overlay, Input } from "react-native-elements";
+import { Entypo } from '@expo/vector-icons'; 
 
 import MapView from "react-native-maps";
-import {Marker} from 'react-native-maps';
+import { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 
 function MapScreen() {
+  const [currentLatitude, setCurrentLatitude] = useState(48.866667);
+  const [currentLongitude, setCurrentLongitude] = useState(2.333333);
 
-    const [currentLatitude, setCurrentLatitude] = useState(48.866667);
-    const [currentLongitude, setCurrentLongitude] = useState(2.333333);
+  console.log("currentLattitude", currentLatitude);
+  console.log("currentLongitude", currentLongitude);
 
-    console.log('currentLattitude', currentLatitude)
-    console.log('currentLongitude', currentLongitude)
-
-    useEffect(() => {
-        async function askPermissions() {
-          var permissions = await Permissions.askAsync(Permissions.LOCATION);
-          console.log('permissions', permissions)
-          if (permissions.status === 'granted') {
-            await Location.watchPositionAsync(
-                {distanceInterval :10},
-                (location) => {
-                    console.log('location',location)
-                    setCurrentLatitude(location.coords.latitude)
-                    setCurrentLongitude(location.coords.longitude)
-                });
+  useEffect(() => {
+    async function askPermissions() {
+      var permissions = await Permissions.askAsync(Permissions.LOCATION);
+      console.log("permissions", permissions);
+      if (permissions.status === "granted") {
+        await Location.watchPositionAsync(
+          { distanceInterval: 10 },
+          (location) => {
+            console.log("location", location);
+            setCurrentLatitude(location.coords.latitude);
+            setCurrentLongitude(location.coords.longitude);
           }
-        }
-        askPermissions();
-      }, []);
+        );
+      }
+    }
+    askPermissions();
+  }, []);
 
   return (
-      
-<MapView style={{ 
-    flex:1,
-    alignItems:"flex-end",
-    justifyContent : "flex-end"
-    }}>
-<Marker
-          coordinate={{
-            latitude: currentLatitude,
-            longitude: currentLongitude,
+    <MapView
+      style={{
+        flex: 1,
+        alignItems: "flex-end",
+        justifyContent: "flex-end",
+      }}
+    >
+      <Marker
+        coordinate={{
+          latitude: currentLatitude,
+          longitude: currentLongitude,
+        }}
+        title="Hello"
+        description="I am here"
+        pinColor="#8525FF"
+      />
+
+      <View
+        style={{
+          alignSelf: "flex-end",
+        }}
+      >
+        <Button
+          icon={{
+            name: 'plus',
+            type: 'entypo',
+            size: 35,
+            color: 'white',
           }}
-          title="Hello"
-          description="I am here"
+          loading={false}
+          loadingProps={{ size: "small", color: "white" }}
+          buttonStyle={{
+            backgroundColor: "#FFD440",
+            borderRadius: 100,
+            borderColor: "white",
+            borderWidth: 2,
+            width: 65,
+            height: 65,
+          }}
+         
+          containerStyle={{
+            marginHorizontal: 30,
+            marginVertical: 30,
+          }}
+          onPress={() => console.log("aye")}
         />
-
-
-      
-<Button
-                title="+"
-                loading={false}
-                loadingProps={{ size: 'small', color: 'white' }}
-                buttonStyle={{
-                  backgroundColor: '#FFD440',
-                  borderRadius: 100,
-                  borderColor:'white',
-                  borderWidth:2,
-                  width:65, 
-                  height:65,     
-                }}
-                titleStyle={{ 
-                    fontWeight: 'bold', fontSize: 30 }}
-                containerStyle={{
-                  marginHorizontal: 30,
-                  marginVertical: 30,
-                }}
-                onPress={() => console.log('aye')}
-              />
-</MapView>
-    
+      </View>
+    </MapView>
   );
 }
 
