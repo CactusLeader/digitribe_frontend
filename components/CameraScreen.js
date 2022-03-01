49168376bdddae630289497d020c2dfbf5bf66c0
Elodie Morel <elodie.morel08@gmail.com>
2022-03-01 12:44:49 +0100

    rajout du bouton et de la fonctionnalité flip

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -1,11 +1,57 @@
 import { Camera } from "expo-camera";
-import { View } from "react-native";
+import { View, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Button } from "react-native-elements";
 
+function CameraScreen(props) {
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
-function CameraScreen (props) {
-    return (<View style={{ flex: 1 }}>
-        <Camera style={{ flex: 1 }}></Camera>
-      </View>)
+  return (
+    <Camera
+      style={{
+        flex: 1,
+        flexDirection: "row",
+      }}
+      type={type}
+    >
+      <TouchableOpacity
+        style={{
+          alignItems: "flex-end",
+          justifyContent: "flex-end",
+          marginBottom: 20,
+        }}
+      >
+        <Button
+          icon={{
+            name: "flip-camera-ios",
+            type: "material-icons",
+            size: 35,
+            color: "white",
+          }}
+          buttonStyle={{
+            backgroundColor: "#FFD440",
+            borderRadius: 100,
+            borderColor: "white",
+            borderWidth: 2,
+            width: 65,
+            height: 65,
+          }}
+          containerStyle={{
+            marginHorizontal: 20,
+            marginVertical: 10,
+          }}
+          onPress={() => {
+            setType(
+              type == Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}
+        />
+      </TouchableOpacity>
+    </Camera>
+  );
 }
 
 export default CameraScreen;
